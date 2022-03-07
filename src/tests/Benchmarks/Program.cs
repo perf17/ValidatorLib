53fe3fe169b11e42a6d744dcb4c2a38c2f0c7da3
root <root@perflens-vm0.qc5yzqq15nnudggvj3gsj05flb.bx.internal.cloudapp.net>
2022-03-07 06:12:51 +0000

    Update benchmark.

--- src/tests/Benchmarks/Program.cs
+++ src/tests/Benchmarks/Program.cs
@@ -7,9 +7,6 @@ namespace Benchmarks
         static void Main(string[] args)
         {
             BenchmarkRunner.Run<StringMethodsBenchmark>();
-            BenchmarkRunner.Run<IntMethodsBenchmark>();
-            BenchmarkRunner.Run<ObjectMethodsBenchmark>();
-            BenchmarkRunner.Run<GuidMethodsBenchmark>();
         }
     }
 }
